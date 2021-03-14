e3d5fff65e4460ed820ca36e240235670c7df8de
Rodrigo Paczkovski <paczweb@gmail.com>
2021-03-14 10:32:43 -0300

    chore: set initial settings

---
+++ src/start.py
@@ -0,0 +1,11 @@
+def soma(num_1: int, num_2: int) -> int:
+    """Soma de dois números
+    :param - num_1: primeiro número da soma
+    :param - num_2: segundo número da soma
+    :return - Soma entre dois números
+    """
+    result = num_1 + num_2
+    return result
+
+
+soma(1, 2)
---
+++ src/start_test.py
@@ -0,0 +1,7 @@
+from .start import soma
+
+
+def test_soma():
+    """ testing soma """
+    result = soma(2, 4)
+    assert result == 6
